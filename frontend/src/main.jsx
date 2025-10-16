877731426ab965761edcf7fb6a4d8a12bf528bf3
Masroor Muntasir <mmun8220@uni.sydney.edu.au>
2025-10-16 16:58:50 +1100

    Alot of frontend done OPENAI API connected, DOLMA is alive

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing.jsx";
+import Signin from "./pages/Signin.jsx";
+import Signup from "./pages/Signup.jsx";
+import Home from "./pages/Home.jsx";
+import Settings from "./pages/Settings.jsx"; // 👈 ADD THIS
+import "./App.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/settings" element={<Settings />} /> {/* 👈 NEW ROUTE */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
