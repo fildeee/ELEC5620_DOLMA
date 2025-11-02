1fdf5ab29390a628e5a02b992d42c013858ee4d6
Kia1902 <vardhan.alaukika@gmail.com>
2025-11-02 20:36:38 +1100

    Wrap routes in DolmaAvatarProvider for context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,18 +6,21 @@ import Signin from "./pages/Signin.jsx";
 import Signup from "./pages/Signup.jsx";
 import Home from "./pages/Home.jsx";
 import Settings from "./pages/Settings.jsx"; // 👈 ADD THIS
+import { DolmaAvatarProvider } from "./context/DolmaAvatarContext.jsx";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/settings" element={<Settings />} /> {/* 👈 NEW ROUTE */}
-      </Routes>
-    </BrowserRouter>
+    <DolmaAvatarProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/settings" element={<Settings />} /> {/* 👈 NEW ROUTE */}
+        </Routes>
+      </BrowserRouter>
+    </DolmaAvatarProvider>
   </React.StrictMode>
 );
